33a07e4afc19774230ef84db9fd01a1188868148
Jieruei Chang <knosmos@gmail.com>
2024-12-03 00:13:56 -0500

    day 3

---
+++ 03/d3.py
@@ -0,0 +1,29 @@
+from aocd import data, submit
+import sys
+from collections import defaultdict
+
+def main(s):
+    r = 0
+    en = True
+    for i in range(len(s)-3):
+        if s[i:i+4] == "do()":
+            en = True
+        if s[i:i+7] == "don't()":
+            en = False
+        if s[i:i+3] == "mul":
+            try:
+                a,b = s[i+4:i+3+s[i+3:].index(")")].split(",")
+                if a.isnumeric() and b.isnumeric():
+                    if en: r += int(a)*int(b)
+            except:pass
+    return r
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == 'p':
+        print(main(open('d3.txt','r').read()))
+    elif sys.argv[1] == 's':
+        a = main(data)
+        print(a)
+        submit(a)
+else:
+    print(main(data))
--- README.md
+++ README.md
@@ -9,3 +9,6 @@ Yet another Advent of Code 2024 repository
 ### Day 2
 1. I check first that the differences between adjacent values are valid, and then check that the sequence is monotonic.
 2. Brute-force over all possible removals (as well as no removals)
+### Day 3
+1. I use a cursed checking function to look for the multiplication commands
+2. Add in an enable flag to turn on and off multiplications
